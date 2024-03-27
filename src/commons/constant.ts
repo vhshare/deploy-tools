dfd343f41e1ee997d6c6032e91aa25ac428d73be
WangDing <fenqing16888@163.com>
2024-03-27 13:12:59 +0800

    1

--- src/commons/constant.ts
+++ src/commons/constant.ts
@@ -3,7 +3,7 @@ import { Key } from "../enums/Enums";
 function genKeyPathMapping(){
   const keyPathMapping = new Map<Key, string[]>();
   keyPathMapping.set(Key.TEST, ['cmd.exe', '/c', 'cd D:\\file\\doc\\fwd && aa10.bat']);
-  keyPathMapping.set(Key.API10, ['cmd.exe',  '/c', 'cd D:\\temp_deploy && api10.bat']);
+  keyPathMapping.set(Key.API10, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && api10.bat']);
   return function(key: Key): string[] {
     return keyPathMapping.get(key) || [];
   }
