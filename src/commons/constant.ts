d1507f657ce2934092cffb39201ce8e94ebe25d2
Steward <weichuan.zhang@gientech.com>
2024-03-27 15:14:11 +0800

    add

--- src/commons/constant.ts
+++ src/commons/constant.ts
@@ -3,7 +3,14 @@ import { Key } from "../enums/Enums";
 function genKeyPathMapping(){
   const keyPathMapping = new Map<Key, string[]>();
   keyPathMapping.set(Key.TEST, ['cmd.exe', '/c', 'cd D:\\file\\doc\\fwd && aa10.bat']);
+  keyPathMapping.set(Key.WEB6, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && web6.bat']);
+  keyPathMapping.set(Key.API6, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && api6.bat']);
+  keyPathMapping.set(Key.WEB9, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && web9.bat']);
+  keyPathMapping.set(Key.API9, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && api9.bat']);
+  keyPathMapping.set(Key.WEB10, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && web10.bat']);
   keyPathMapping.set(Key.API10, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && api10.bat']);
+  keyPathMapping.set(Key.esWEB41, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && es-web41.bat']);
+  keyPathMapping.set(Key.esAPI41, ['cmd.exe',  '/c', 'D: && cd D:\\temp_deploy && es-api41.bat']);
   return function(key: Key): string[] {
     return keyPathMapping.get(key) || [];
   }
--- src/enums/Enums.ts
+++ src/enums/Enums.ts
@@ -1,4 +1,11 @@
 export enum Key {
   TEST = "pgD0r1f57slk1qg8",
-  API10 = "U6R0gK3jVLX7DmlN"
+  WEB6 = "jbRQb9D8pWQVHPXe",
+  API6 = "jCM5DuxYF3VSxZ3j",
+  WEB9 = "Nd43NsB6EgjXrtcq",
+  API9 = "C2bUSZJfrcd2kK9H",
+  WEB10 = "ZiKY8xp68AawrMKF",
+  API10 = "U6R0gK3jVLX7DmlN",
+  esWEB41 = "xsPjGhR8FVcMc7yK",
+  esAPI41 = "iT29UzqVW253C4tW",
 }
